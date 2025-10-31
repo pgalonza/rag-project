449cdf7e1e755419bef6a411293306447974e927
Peter Galonza <p.galonza@evaron.ru>
2025-10-31 18:04:43 +0300

    feature: disable calculate_cosine_similarity, it`s not ready

--- llm-audit/app/routes/estimation.py
+++ llm-audit/app/routes/estimation.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request, current_app
-from app.services import cosine_similarity, tokenizer
+from app.services import tokenizer
 from werkzeug.exceptions import BadRequest
 from concurrent.futures import ThreadPoolExecutor
 
@@ -52,7 +52,7 @@ def estimate_similarity():
         current_app.logger.warning("Empty %s provided", e)
         raise BadRequest("Both %s must be provided and non-empty" % e)
 
-    executor.submit(cosine_similarity.calculate_cosine_similarity(augmented_prompt, generated_text))
+    # executor.submit(cosine_similarity.calculate_cosine_similarity(augmented_prompt, generated_text))
     executor.submit(tokenizer.get_tokens(generated_text, current_app.config['YC_FOLDER_ID'], current_app.config['MODEL_NAME']))
     return jsonify({'status': 'ok'})
     # try:
