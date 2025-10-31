597f29be37a527ec4cd823e7aa1c3b0f6282d060
Peter Galonza <p.galonza@evaron.ru>
2025-10-31 17:36:57 +0300

    feature: use docker for augmented-generation

--- augmented-generation/config.py
+++ augmented-generation/config.py
@@ -2,12 +2,11 @@ import os
 
 class Config:
     """Base configuration class."""
-    SERVER_NAME = '127.0.0.1:8002'
+    SERVER_NAME = os.environ.get("SERVER_NAME", '127.0.0.1:8002')
     COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "data")
     YC_FOLDER_ID = os.environ.get("YC_FOLDER_ID")
     MODEL_NAME = "yandexgpt-lite"
     MODEL_TEMPERATURE = 0.5
-    QDRANT_URL = os.environ.get("QDRANT_URL")
     DOCUMENTS_SEARCH_URL = os.environ.get("DOCUMENTS_SEARCH_URL", "http://127.0.0.1:8001/api/v1/documents/search")
     LLM_ESTIMATION_URL = os.environ.get("LLM_ESTIMATION_URL", "http://127.0.0.1:8003/api/v1/estimate")
     ASSISTENT_ROLE = f"{os.environ.get('ASSISTENT_ROLE', "You're a friendly assistant.")}:\n{{context}}"
