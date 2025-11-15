47da1b8c2b148900161d5d3ac180017a3b7ec9fa
Peter Galonza <p.galonza@evaron.ru>
2025-11-15 21:54:58 +0300

    feature: optimize vector store initialization and add db timeout/retry config

--- data-vector-manager/app/db.py
+++ data-vector-manager/app/db.py
@@ -4,6 +4,7 @@ from langchain_qdrant import QdrantVectorStore
 from langchain_community.embeddings.yandex import YandexGPTEmbeddings
 from flask import current_app, g
 
+_vector_store = None
 
 def init_app(app):
     app.teardown_appcontext(close_db)
@@ -18,7 +19,10 @@ def create_all():
 
 def get_db():
     if 'db' not in g:
-        g.db = QdrantClient(url=current_app.config['QDRANT_URL'])
+        g.db = QdrantClient(
+            url=current_app.config['QDRANT_URL'],
+            timeout=30,
+        )
     return g.db
 
 
@@ -28,41 +32,26 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+def get_vector_store():
+    global _vector_store
+    if _vector_store is None:
+        db = get_db()
+        embeddings = YandexGPTEmbeddings()
+        _vector_store = QdrantVectorStore(
+            client=db,
+            collection_name=current_app.config['COLLECTION_NAME'],
+            embedding=embeddings,
+        )
+    return _vector_store
+
 def add_documents(documents):
-    db = get_db()
-    embeddings = YandexGPTEmbeddings()
-    vector_store = QdrantVectorStore(
-        client=db,
-        collection_name=current_app.config['COLLECTION_NAME'],
-        embedding=embeddings,
-    )
-    result = vector_store.add_documents(documents=documents)
-    return result
+    vector_store = get_vector_store()
+    return vector_store.add_documents(documents=documents)
 
 def get_documents(ids):
-    db = get_db()
-
-    embeddings = YandexGPTEmbeddings()
-    vector_store = QdrantVectorStore(
-        client=db,
-        collection_name=current_app.config['COLLECTION_NAME'],
-        embedding=embeddings,
-    )
-
-    documents = vector_store.get_by_ids(ids)
-
-    return documents
+    vector_store = get_vector_store()
+    return vector_store.get_by_ids(ids)
 
 def search_documents(query, number):
-    db = get_db()
-
-    embeddings = YandexGPTEmbeddings()
-    vector_store = QdrantVectorStore(
-        client=db,
-        collection_name=current_app.config['COLLECTION_NAME'],
-        embedding=embeddings,
-    )
-
-    documents = vector_store.similarity_search(query, number)
-
-    return documents
+    vector_store = get_vector_store()
+    return vector_store.similarity_search(query, number)
